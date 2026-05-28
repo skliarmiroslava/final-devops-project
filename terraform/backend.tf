73a3e591ba7789261e8ae55e9076d11066637988
skliarmiroslava <skliarmiroslava7@gmail.com>
2026-05-28 10:05:20 +0300

    Improve backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,14 +1,19 @@
-#terraform {
-#  backend "s3" {
-#    bucket         = "danit-devops-tf-state"
-#    key            = "eks/terraform.tfstate"
-#    encrypt        = true
+#resource "aws_s3_bucket" "devops_tf_state" {
+#    bucket = "miroslava-devops-tf-state"
+
+#}
+
+terraform {
+  backend "s3" {
+    bucket         = "miroslava-devops-tf-state"
+    key            = "eks/terraform.tfstate"
+    encrypt        = true
 #    dynamodb_table = "lock-tf-eks"
     # dynamo key LockID
     # Params tekan from -backend-config when terraform init
-    #region = 
+    region = "eu-central-1"
     #profile = 
-#  }
-#}
+  }
+}
 
 
